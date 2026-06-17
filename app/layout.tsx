3fa6ec3f4d36c852234b2a642cacd65eedfa6c19
Arman Dezfuli <armandezfuli@proton.me>
2026-06-17 20:15:06 +0330

    refactor: clean up imports and formatting in layout and dropdown menu components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { cn } from "@/lib/utils"
 import Providers from "./providers"
 import Navbar from "@/components/layout/Navbar/Navbar"
 
