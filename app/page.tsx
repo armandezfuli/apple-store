34df2ef5249973f6e219d958e383fa3cc6b766c6
Arman Dezfuli <armandezfuli@proton.me>
2026-06-17 18:09:32 +0330

    feat: add responsive navbar and theme swiching

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image"
+
+const iphones = [
+    "/images/iphones/iphone_17.jpg",
+    "/images/iphones/iphone_17e.jpg",
+    "/images/iphones/iphone_17pro.jpg",
+    "/images/iphones/iphone_air.jpg",
+]
 
 export default function Home() {
     return (
-        <>
-            <h1>Home page</h1>
-            <div className="max-wsm">
-                <Button>Click me</Button>
-            </div>
-        </>
+        <div className="mt-22 mx-auto grid grid-cols-4 gap-6 ">
+            {iphones.map((iphone) => (
+                <Image key={iphone} src={iphone} alt="iPhone" width={344} height={366} className="rounded-4xl" />
+            ))}
+        </div>
     )
 }
